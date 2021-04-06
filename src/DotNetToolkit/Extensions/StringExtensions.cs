085c5410405d48e1b100c0fc0f1422c1d1baebd6
Claudiu <cristian.danila@microfocus.com>
2021-04-06 21:13:56 +0300

    Add IsEmptyOrWhiteSpace extension for string

---
+++ src/DotNetToolkit/Extensions/StringExtensions.cs
@@ -0,0 +1,54 @@
+﻿/*********************************************************************************
+* The MIT License(MIT)                                                           *
+*                                                                                *
+* Copyright(c) Open Source Software Initiative Contributors                      *
+*                                                                                *
+* Permission is hereby granted, free of charge, to any person obtaining a copy   *
+* of this software and associated documentation files (the "Software"), to deal  *
+* in the Software without restriction, including without limitation the rights   *
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      *
+* copies of the Software, and to permit persons to whom the Software is          *
+* furnished to do so, subject to the following conditions:                       *
+*                                                                                *
+* The above copyright notice and this permission notice shall be included in all *
+* copies or substantial portions of the Software.                                *
+*                                                                                *
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       *
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         *
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *
+* SOFTWARE.                                                                      *
+*********************************************************************************/
+
+
+using System;
+using System.IO;
+using System.Linq;
+
+namespace System
+{
+    /// <summary>
+    /// Provides a set of static (Shared in Visual Basic) methods for working with specific
+    /// kinds of <see cref="string"/> instances.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Gets the directory path of the specified assembly.
+        /// </summary>
+        /// <param name="value">The string to test.</param>
+        /// <returns><c>true</c> if the <paramref name="value"/> consists exclusively of white-space characters, <c>false</c> otherwise.</returns>
+        ///
+        /// <exception cref="ArgumentNullException"><paramref name="value"/> is null.</exception>
+        public static bool IsEmptyOrWhiteSpace(this string value)
+        {
+            if(value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+            return value.All(char.IsWhiteSpace);
+        }
+    }
+}
